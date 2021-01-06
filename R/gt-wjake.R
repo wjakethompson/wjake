394c76da8bfecfcea3e19b72bf3c54fc0aeab6b1
Jake Thompson <wjakethompson@gmail.com>
2021-01-06 10:22:51 -0600

    update gt theme

--- R/gt-wjake.R
+++ R/gt-wjake.R
@@ -8,36 +8,38 @@
 #'
 #' @return An object of class `gt_tbl`.
 #' @export
-gt_theme_wjake <- function(data, bg_color = "#F0F0F0", ...) {
-  data %>%
-    opt_all_caps() %>%
-    opt_table_font(font = list(google_font("Source Sans Pro"),
-                               default_fonts())) %>%
-    tab_options(column_labels.background.color = bg_color,
-                column_labels.border.top.width = px(3),
-                column_labels.border.top.color = bg_color,
-                table.border.top.width = px(1),
-                table.border.bottom.width = px(1),
-                heading.align = "left",
-                heading.background.color = bg_color,
-                source_notes.padding = px(10),
-                source_notes.border.lr.width = px(0),
-                source_notes.font.size = 12,
-                source_notes.background.color = bg_color,
-                table.font.size = 16,
-                data_row.padding = px(3),
-                table.background.color = bg_color,
-                ...) %>%
-    tab_style(style = cell_borders(sides = "bottom", color = "transparent",
-                                   weight = px(2)),
-              locations = cells_body(columns = TRUE,
-                                     rows = nrow(data$`_data`))) %>%
-    tab_style(style = list(cell_text(weight = "bold", align = "center",
-                                     v_align = "middle"),
-                           cell_borders(sides = "bottom", color = "black",
-                                        weight = px(3))),
-              locations = cells_column_labels(TRUE)) %>%
-    tab_style(style = cell_fill(color = bg_color),
-              locations = list(cells_body(columns = gt::everything(),
-                                          rows = gt::everything())))
+gt_theme_wjake <- function(gt_dat, bg_color = "#F0F0F0", ...) {
+  gt_dat %>%
+    gt::opt_all_caps() %>%
+    gt::opt_table_font(font = list(gt::google_font("Source Sans Pro"),
+                                   gt::default_fonts())) %>%
+    gt::tab_options(column_labels.background.color = bg_color,
+                    column_labels.border.top.width = gt::px(3),
+                    column_labels.border.top.color = bg_color,
+                    table.border.top.width = gt::px(1),
+                    table.border.bottom.width = gt::px(1),
+                    heading.align = "left",
+                    heading.background.color = bg_color,
+                    source_notes.padding = gt::px(10),
+                    source_notes.border.lr.width = gt::px(0),
+                    source_notes.font.size = 12,
+                    source_notes.background.color = bg_color,
+                    table.font.size = 16,
+                    data_row.padding = gt::px(3),
+                    table.background.color = bg_color,
+                    ...) %>%
+    gt::tab_style(style = gt::cell_borders(sides = "bottom",
+                                           color = "transparent",
+                                           weight = gt::px(2)),
+                  locations = gt::cells_body(columns = TRUE,
+                                             rows = nrow(gt_dat$`_data`))) %>%
+    gt::tab_style(style = list(gt::cell_text(weight = "bold", align = "center",
+                                             v_align = "middle"),
+                               gt::cell_borders(sides = "bottom",
+                                                color = "black",
+                                                weight = gt::px(3))),
+                  locations = gt::cells_column_labels(TRUE)) %>%
+    gt::tab_style(style = gt::cell_fill(color = bg_color),
+                  locations = list(gt::cells_body(columns = gt::everything(),
+                                                  rows = gt::everything())))
 }
