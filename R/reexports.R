1f4da26e1d87d4f2544070648424683fb5606b96
Jake Thompson <wjakethompson@gmail.com>
2021-01-05 18:55:53 -0600

    re-export some theme modifiers

---
+++ R/reexports.R
@@ -0,0 +1,24 @@
+#' @importFrom hrbrthemes scale_x_comma
+#' @export
+hrbrthemes::scale_x_comma
+#' @importFrom hrbrthemes scale_y_comma
+#' @export
+hrbrthemes::scale_y_comma
+#' @importFrom hrbrthemes scale_x_percent
+#' @export
+hrbrthemes::scale_x_percent
+#' @importFrom hrbrthemes scale_y_percent
+#' @export
+hrbrthemes::scale_y_percent
+#' @importFrom ratlas scale_color_okabeito
+#' @export
+ratlas::scale_color_okabeito
+#' @importFrom ratlas scale_colour_okabeito
+#' @export
+ratlas::scale_colour_okabeito
+#' @importFrom ratlas scale_fill_okabeito
+#' @export
+ratlas::scale_fill_okabeito
+#' @importFrom ratlas scale_okabeito
+#' @export
+ratlas::scale_okabeito
