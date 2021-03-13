d0acea14cf4c354b24d09bfc9af4ee3f2e601edf
Jake Thompson <wjakethompson@gmail.com>
2021-03-12 18:10:11 -0600

    add draft consulting agreement

--- R/render-rmd.R
+++ R/render-rmd.R
@@ -43,3 +43,50 @@ consulting_report <- function(...) {
 
   base
 }
+
+
+#' Create an R Markdown PDF Consulting Agreement
+#'
+#' This is a function called in the output of the yaml of the Rmd file to
+#' specify using the standard consulting report PDF formatting.
+#'
+#' @param ... Arguments to be passed to `[bookdown::pdf_document2]`
+#'
+#' @return A modified `pdf_document2` with the standard consulting report
+#'   formatting.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   output: wjake::consulting_agreement
+#' }
+consulting_agreement <- function(...) {
+  agreement_template <- find_resource("consulting-agreement", "template.tex")
+  base <- bookdown::pdf_document2(template = agreement_template,
+                                  latex_engine = "xelatex",
+                                  citation_package = "biblatex",
+                                  keep_tex = TRUE, number_sections = FALSE, ...)
+
+  # nolint start
+  base$knitr$opts_chunk$comment <- "#>"
+  base$knitr$opts_chunk$message <- FALSE
+  base$knitr$opts_chunk$warning <- FALSE
+  base$knitr$opts_chunk$error <- FALSE
+  base$knitr$opts_chunk$echo <- FALSE
+  base$knitr$opts_chunk$cache <- FALSE
+  base$knitr$opts_chunk$fig.width <- 8
+  base$knitr$opts_chunk$fig.asp <- 0.618
+  base$knitr$opts_chunk$fig.ext <- "pdf"
+  base$knitr$opts_chunk$fig.align <- "center"
+  base$knitr$opts_chunk$fig.retina <- 3
+  base$knitr$opts_chunk$fig.path <- "figures/"
+  base$knitr$opts_chunk$fig.pos <- "H"
+  base$knitr$opts_chunk$out.extra <- ""
+  base$knitr$opts_chunk$out.width <- "100%"
+  base$knitr$opts_chunk$fig.show <- "hold"
+  # nolint end
+
+  base$knitr$knit_hooks$plot <- ratlas:::hook_tex_plot_rat
+
+  base
+}
