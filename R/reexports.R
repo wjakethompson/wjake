972a6289c3f92074f8495c78de3bc601564d9884
Jake Thompson <wjakethompson@gmail.com>
2023-11-08 12:59:50 -0600

    reexport more formatting functions

--- R/reexports.R
+++ R/reexports.R
@@ -14,45 +14,83 @@ hrbrthemes::scale_y_percent
 #' @importFrom ratlas palette_okabeito
 #' @export
 ratlas::palette_okabeito
+
 #' @importFrom ratlas palette_okabeito_black
 #' @export
 ratlas::palette_okabeito_black
+
 #' @importFrom ratlas scale_color_okabeito
 #' @export
 ratlas::scale_color_okabeito
+
 #' @importFrom ratlas scale_color_okabeito
 #' @export
 ratlas::scale_color_okabeito
+
 #' @importFrom ratlas scale_colour_okabeito
 #' @export
 ratlas::scale_colour_okabeito
+
 #' @importFrom ratlas scale_fill_okabeito
 #' @export
 ratlas::scale_fill_okabeito
+
 #' @importFrom ratlas scale_okabeito
 #' @export
 ratlas::scale_okabeito
+
 #' @importFrom ratlas ggsave2
 #' @export
 ratlas::ggsave2
+
 #' @importFrom ratlas fmt_count
 #' @export
 ratlas::fmt_count
+
 #' @importFrom ratlas fmt_digits
 #' @export
 ratlas::fmt_digits
+
 #' @importFrom ratlas fmt_italic
 #' @export
 ratlas::fmt_italic
-#' @importFrom ratlas fmt_kbl
+
+#' @importFrom ratlas fmt_leading_zero
 #' @export
-ratlas::fmt_kbl
-#' @importFrom ratlas fmt_kbl_header
+ratlas::fmt_leading_zero
+
+#' @importFrom ratlas fmt_minus
 #' @export
-ratlas::fmt_kbl_header
-#' @importFrom ratlas fmt_table
+ratlas::fmt_minus
+
+#' @importFrom ratlas fmt_replace_na
 #' @export
-ratlas::fmt_table
+ratlas::fmt_replace_na
+
+#' @importFrom ratlas fmt_corr
+#' @export
+ratlas::fmt_corr
+
+#' @importFrom ratlas fmt_prop
+#' @export
+ratlas::fmt_prop
+
 #' @importFrom ratlas fmt_prop_pct
 #' @export
 ratlas::fmt_prop_pct
+
+#' @importFrom ratlas fmt_italic
+#' @export
+ratlas::fmt_italic
+
+#' @importFrom ratlas fmt_kbl
+#' @export
+ratlas::fmt_kbl
+
+#' @importFrom ratlas fmt_kbl_header
+#' @export
+ratlas::fmt_kbl_header
+
+#' @importFrom ratlas fmt_table
+#' @export
+ratlas::fmt_table
