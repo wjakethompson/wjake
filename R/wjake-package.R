db0b9c3f34993cf1da28c172571c10160a8be7a8
Jake Thompson <wjakethompson@gmail.com>
2024-06-18 10:48:14 -0500

    update deprecated package doc type

--- R/wjake-package.R
+++ R/wjake-package.R
@@ -3,12 +3,14 @@
 #' Personal package for helping with R setup and random tasks that I Google too
 #' often.
 #'
-#' @docType package
-#' @name wjake
-#' @aliases wjake
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
 #' @import grDevices
 #' @import ggplot2
 #' @importFrom rlang .data
+## usethis namespace: end
 NULL
 
 ## Make R CMD Check go away
