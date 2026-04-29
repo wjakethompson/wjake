0ed160bc11730f58eb774cabf9bea61abb245b1f
Jake Thompson <wjakethompson@gmail.com>
2026-04-29 12:46:58 -0500

    update repos for CRAN citation

--- R/write-bib.R
+++ R/write-bib.R
@@ -24,15 +24,12 @@ write_pkg_bib <- function(pkg, file, update = FALSE) {
 
 cite_package <- function(x) {
   pkg_name <- stringr::str_replace(x, "^.*/", "")
-  print(pkg_name)
   meta <- utils::packageDescription(pkg_name)
-  print(meta)
   repo <- meta$Repository
-  print(repo)
 
   pkg_cite <- if (is.null(repo) && meta$RemoteType == "github") {
     cite_github_pkg(meta)
-  } else if (repo == "CRAN") {
+  } else if (repo %in% c("CRAN", "RSPM")) {
     cite_cran_pkg(meta)
   }
 
